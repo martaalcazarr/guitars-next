f7740aa84ad0a8577b22f64757585c087f82b1ca
martaalcazarr <martaalcazarredondo@gmail.com>
2023-07-25 22:32:17 -0400

    add dynamic routing

--- pages/guitars/[url].js
+++ pages/guitars/[url].js
@@ -1,9 +1,46 @@
-import { useRouter } from "next/router"
-export default function Product() {
-  const router = useRouter()
+
+export default function Product({guitar}) {
+
   return (
     <div>
       [url]
     </div>
   )
 }
+
+export async function getStaticPaths(){
+  const response = await fetch(`${process.env.API_URL}/guitars`)
+  const {data} = await response.json()
+
+  const paths = data.map(guitar => ({
+    params: {
+      url: guitar.attributes.url
+    }
+  }))
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params: {url}}){
+  const response = await fetch(`${process.env.API_URL}/guitars?filters[url]={url}&populate=imagen`)
+  const {data: guitar} = await response.json()
+  console.log(data)
+  return {
+    props: {
+      guitar
+    }
+  }
+}
+
+// export async function getServerSideProps({query: {url}}){
+//   const response = await fetch(`${process.env.API_URL}/guitars?filters[url]={url}&populate=imagen`)
+//   const {data: guitar} = await response.json()
+//   console.log(data)
+//   return {
+//     props: {
+//       guitar
+//     }
+//   }
+// }
